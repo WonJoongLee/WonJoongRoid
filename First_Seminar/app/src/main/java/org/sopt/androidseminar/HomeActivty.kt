f948ebc53a04ebef63ec7c283995f631d645b1f2
Lee WonJoong <realjoong@gmail.com>
2021-04-05 14:30:22 +0900

    HomeActivity 추가, 내 사진 추가
    - SignIn, SignUp, Home Activity xml 작업 끝.

---
+++ First_Seminar/app/src/main/java/org/sopt/androidseminar/HomeActivty.kt
@@ -0,0 +1,11 @@
+package org.sopt.androidseminar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_activty)
+    }
+}
