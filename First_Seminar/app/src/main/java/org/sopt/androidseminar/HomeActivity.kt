445b7915533b8b4369311b7d0a7b776be772f3b4
Lee WonJoong <realjoong@gmail.com>
2021-04-05 17:19:21 +0900

    Home Activity Level 1 완료
    - xml에 `constraintDimesionRatio` 1:1로 설정
    - 생명주기 로그 추가

--- First_Seminar/app/src/main/java/org/sopt/androidseminar/HomeActivity.kt
+++ First_Seminar/app/src/main/java/org/sopt/androidseminar/HomeActivity.kt
@@ -2,10 +2,49 @@ package org.sopt.androidseminar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.sopt.androidseminar.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+    private val activityName = "HomeActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.e(activityName, "onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(activityName, "onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e(activityName, "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(activityName, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(activityName, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(activityName, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(activityName, "onDestroy")
+    }
+
 }
