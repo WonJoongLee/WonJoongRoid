90767cba1e635ed4a6d53c93c7578e1b3a661014
Lee WonJoong <realjoong@gmail.com>
2021-04-11 15:08:04 +0900

    (#3) 함수로 깔끔한 코드 생성.
    - 기존에 onCreate()에서 리사이클러 뷰 세팅을 처음부터 끝까지 다 해줬는데, `private fun setRepoRv` 함수를 생성하여 코드를 깔끔하게 처리했음.

--- Second_Seminar/app/src/main/java/org/sopt/androidseminar/ui/HomeActivity.kt
+++ Second_Seminar/app/src/main/java/org/sopt/androidseminar/ui/HomeActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.androidseminar.R
@@ -14,7 +13,7 @@ import org.sopt.androidseminar.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityHomeBinding
+    private lateinit var binding: ActivityHomeBinding
     private val ACTIVITYNAME = "HomeActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,31 +22,42 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
         Log.d(ACTIVITYNAME, "onCreate")
 
+        setRepoRv()
+
+        moreButtonClickedEvent()
+    }
+
+    private fun setRepoRv() {
         var repoList = mutableListOf<RepositoryInfo>()
         val repoAdapter = RepositoryAdapter()
         val repoRecyclerView = findViewById<RecyclerView>(R.id.rv_repository)
         repoList = repoDataInput(repoList)
         repoAdapter.data = repoList
-        repoRecyclerView.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        repoRecyclerView.layoutManager =
+            LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
         repoRecyclerView.adapter = repoAdapter
         repoRecyclerView.setHasFixedSize(false)
-
-        moreButtonClickedEvent()
     }
 
-    private fun moreButtonClickedEvent(){
+    private fun moreButtonClickedEvent() {
         binding.btMore.setOnClickListener {
             val intent = Intent(this, FragmentActivity::class.java)
             startActivity(intent)
         }
     }
 
-    private fun repoDataInput(repoList : MutableList<RepositoryInfo>) : MutableList<RepositoryInfo>{
-        for(i in 1..10){
+    private fun repoDataInput(repoList: MutableList<RepositoryInfo>): MutableList<RepositoryInfo> {
+        for (i in 1..10) {
             repoList.add(RepositoryInfo("레포지토리 이름".plus(i), "레포지토리 설명".plus(i), "레포지토리 언어".plus(i)))
         }
         //Repository 이름이나 설명이 긴 경우 처리를 보여주기 위해 임시로 추가
-        repoList.add(RepositoryInfo("레포지토리 이름레포지토리 이름레포지토리 이름레포지토리 이름레포지토리 이름레포지토리 이름레포지토리 이름", "레포지토리 설명레포지토리 설명레포지토리 설명레포지토리 설명레포지토리 설명", "레포지토리 언어"))
+        repoList.add(
+            RepositoryInfo(
+                "레포지토리 이름레포지토리 이름레포지토리 이름레포지토리 이름레포지토리 이름레포지토리 이름레포지토리 이름",
+                "레포지토리 설명레포지토리 설명레포지토리 설명레포지토리 설명레포지토리 설명",
+                "레포지토리 언어"
+            )
+        )
         return repoList
     }
 
