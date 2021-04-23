24c500beff84e6c1f8f3ead4853ebbe80f7e4610
Lee WonJoong <realjoong@gmail.com>
2021-04-23 12:41:04 +0900

    (#6) Recyclerview에 광고 item 추가
    - GridLayout은 구현되었음.
    - Level 2-2 Recycler View에 여러 view 보여주기 과제 중

--- Second_Seminar/app/src/main/java/org/sopt/androidseminar/adapters/FollowerAdapter.kt
+++ Second_Seminar/app/src/main/java/org/sopt/androidseminar/adapters/FollowerAdapter.kt
@@ -6,20 +6,34 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.androidseminar.R
 import org.sopt.androidseminar.data.FollowerInfo
+import org.sopt.androidseminar.data.ViewTypeInfo
+import org.sopt.androidseminar.databinding.ItemAdvertisementBinding
 import org.sopt.androidseminar.databinding.ItemFollowerBinding
 
 class FollowerAdapter(private val data: List<FollowerInfo>) :
-    RecyclerView.Adapter<FollowerAdapter.FollowerViewHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowerViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val binding: ItemFollowerBinding =
-            DataBindingUtil.inflate(layoutInflater, R.layout.item_follower, parent, false)
+        lateinit var binding: ItemFollowerBinding
+        when(viewType){
+            ViewTypeInfo.NORMAL_CONTENT -> {
+                binding = DataBindingUtil.inflate(layoutInflater, R.layout.item_follower, parent, false)
+            }
+            ViewTypeInfo.AD_CONTENT -> {
+                binding = DataBindingUtil.inflate(layoutInflater, R.layout.item_advertisement, parent, false)
+            }
+        }
         return FollowerViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: FollowerViewHolder, position: Int) {
-        holder.bind(data[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        //holder.bind(data[position])
+        if(holder is FollowerViewHolder){
+            holder.bind(data[position])
+        }else if(holder is AdViewHolder){
+            holder.bind()
+        }
     }
 
     override fun getItemCount(): Int = data.size
@@ -31,6 +45,14 @@ class FollowerAdapter(private val data: List<FollowerInfo>) :
                 name = followerInfo.followerName
                 githubNickName = followerInfo.followerGithubId
             }
+        }
+    }
+
+    class AdViewHolder(private val binding: ItemAdvertisementBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(){
+           binding.apply {
+               adContent = "광고!"
+           }
         }
     }
 }
---
+++ Second_Seminar/app/src/main/java/org/sopt/androidseminar/data/ViewTypeInfo.kt
@@ -0,0 +1,8 @@
+package org.sopt.androidseminar.data
+
+class ViewTypeInfo {
+    companion object{
+        const val NORMAL_CONTENT = 0
+        const val AD_CONTENT = 1
+    }
+}
