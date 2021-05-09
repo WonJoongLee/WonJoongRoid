5fd505aa5ca231371b86091fff3629201edb3c28
Lee WonJoong <realjoong@gmail.com>
2021-05-09 10:49:42 +0900

    (#10) Retrofit 위한 interface, data class 추가

---
+++ Fourth_Seminar/app/src/main/java/org/sopt/androidseminar/api/SoptService.kt
@@ -0,0 +1,13 @@
+package org.sopt.androidseminar.api
+
+import org.sopt.androidseminar.data.request.RequestLoginData
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface SoptService {
+    @POST("/login/signin")
+    fun postLogin(
+        @Body body:RequestLoginData
+    ) : Call<RequestLoginData>
+}
