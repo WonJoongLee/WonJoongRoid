7f66c3f6b36494ce7cb5d7a79a4d21498fb0f03a
Lee WonJoong <realjoong@gmail.com>
2021-05-14 22:41:23 +0900

    (#10) Signup Call 객체 생성

--- Fourth_Seminar/app/src/main/java/org/sopt/androidseminar/api/soptlogin/SoptService.kt
+++ Fourth_Seminar/app/src/main/java/org/sopt/androidseminar/api/soptlogin/SoptService.kt
@@ -1,7 +1,9 @@
 package org.sopt.androidseminar.api.soptlogin
 
 import org.sopt.androidseminar.data.request.RequestLoginData
+import org.sopt.androidseminar.data.request.RequestSignupData
 import org.sopt.androidseminar.data.response.ResponseLoginData
+import org.sopt.androidseminar.data.response.ResponseSignupData
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -9,6 +11,11 @@ import retrofit2.http.POST
 interface SoptService {
     @POST("/login/signin")
     fun postLogin(
-        @Body body:RequestLoginData
-    ) : Call<ResponseLoginData>
+        @Body body: RequestLoginData
+    ): Call<ResponseLoginData>
+
+    @POST("/login/signup")
+    fun postSignUp(
+        @Body body: RequestSignupData
+    ): Call<ResponseSignupData>
 }
--- Fourth_Seminar/app/src/main/java/org/sopt/androidseminar/ui/SignUpActivity.kt
+++ Fourth_Seminar/app/src/main/java/org/sopt/androidseminar/ui/SignUpActivity.kt
@@ -4,10 +4,17 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import org.sopt.androidseminar.api.ServiceCreator
+import org.sopt.androidseminar.data.request.RequestSignupData
+import org.sopt.androidseminar.data.response.ResponseSignupData
 import org.sopt.androidseminar.util.LifecycleObserver
 import org.sopt.androidseminar.databinding.ActivitySignUpBinding
 import org.sopt.androidseminar.util.toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -27,8 +34,34 @@ class SignUpActivity : AppCompatActivity() {
             if (isGithubInfoEditTextEmpty()) {
                 toast("빈 칸이 있는지 확인해주세요")
             } else {
+                val requestSignUpData = RequestSignupData(
+                    email = binding.etGithubEmail.text.toString(),
+                    password = binding.etGithubPw.text.toString(),
+                    sex = "0",
+                    nickname = binding.etGithubNickname.text.toString(),
+                    phone = "010-1234-5678",
+                    birth = "1234-56-78"
+                )
+                val call: Call<ResponseSignupData> =
+                    ServiceCreator.soptService.postSignUp(requestSignUpData)
+                call.enqueue(object : Callback<ResponseSignupData> {
+                    override fun onResponse(
+                        call: Call<ResponseSignupData>,
+                        response: Response<ResponseSignupData>
+                    ) {
+                        if (response.isSuccessful) {
+                            val data = response.body()?.responseData
+                            toast(data?.nickname ?: "")
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ResponseSignupData>, t: Throwable) {
+                        Log.e("NetworkFailure", "error:$t")
+                    }
+
+                })
                 val intent = Intent(this, SignUpActivity::class.java)
-                intent.putExtra("githubID", binding.etGithubId.text.toString())
+                intent.putExtra("githubID", binding.etGithubNickname.text.toString())
                 intent.putExtra("pw", binding.etGithubPw.text.toString())
                 setResult(Activity.RESULT_OK, intent)
                 finish()
@@ -38,5 +71,5 @@ class SignUpActivity : AppCompatActivity() {
 
     // 하나라도 비어있지 않은 edit text가 있으면 true 반환, 모두 채워져 있다면 false 반환
     private fun isGithubInfoEditTextEmpty(): Boolean =
-        binding.etGithubName.text.isNullOrBlank() || binding.etGithubId.text.isNullOrBlank() || binding.etGithubPw.text.isNullOrBlank()
+        binding.etGithubEmail.text.isNullOrBlank() || binding.etGithubNickname.text.isNullOrBlank() || binding.etGithubPw.text.isNullOrBlank()
 }
